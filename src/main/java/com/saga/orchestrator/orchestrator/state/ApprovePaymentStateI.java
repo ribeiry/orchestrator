f2748429c2d0850c45b6e9bcc3c838555a7bcdcf
Augusto Ribeiro <augustoribeiro@Augustos-MacBook-Pro.local>
2024-01-04 19:03:42 -0300

    removing comentary

--- src/main/java/com/saga/orchestrator/orchestrator/state/ApprovePaymentStateI.java
+++ src/main/java/com/saga/orchestrator/orchestrator/state/ApprovePaymentStateI.java
@@ -18,7 +18,7 @@ public class ApprovePaymentStateI implements IOrderState {
     public void next(OrderState orderState, Issue issue) {
 
         TransportServices transportServices = new TransportServices();
-        //TODO calcula frete
+
         orderState.nextState(issue);
         PaymentServices paymentServices = new PaymentServices();
         paymentServices.payOrder(issue);
