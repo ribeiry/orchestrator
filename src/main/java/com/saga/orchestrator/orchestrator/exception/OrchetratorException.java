0c448085c680be5ff944b906ae64809e099f385e
WebertonSilva <weberton.fsilva@gmail.com>
2024-03-11 22:11:17 -0300

    Ajustes no código do pedido quando retorna null TODO

--- src/main/java/com/saga/orchestrator/orchestrator/exception/OrchetratorException.java
+++ src/main/java/com/saga/orchestrator/orchestrator/exception/OrchetratorException.java
@@ -1,2 +1,29 @@
-package com.saga.orchestrator.orchestrator.exception;public class OrchetratorException {
+package com.saga.orchestrator.orchestrator.exception;
+
+public class OrchetratorException extends  Exception{
+
+    private int httpstatuscode;
+
+    private String httpstatusmessage;
+
+    public OrchetratorException(int httpstatuscode, String httpstatusmessage){
+        super(httpstatusmessage);
+        this.httpstatuscode = httpstatuscode;
+    }
+
+    public int getHttpstatuscode() {
+        return httpstatuscode;
+    }
+
+    public void setHttpstatuscode(int httpstatuscode) {
+        this.httpstatuscode = httpstatuscode;
+    }
+
+    public String getHttpstatusmessage() {
+        return httpstatusmessage;
+    }
+
+    public void setHttpstatusmessage(String httpstatusmessage) {
+        this.httpstatusmessage = httpstatusmessage;
+    }
 }
